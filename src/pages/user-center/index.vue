8d84ae887d941470efdadf29a92e299d180f0042
mohong <arthas_qin@qq.com>
2018-04-28 14:37:40 +0800

    增加个人中心头像

--- src/pages/user-center/index.vue
+++ src/pages/user-center/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <div class="header">
-        <img class="avatar" v-if="userInfo" src="userInfo.avatar" alt="">
-        <div v-else class="cover">登录</div>
+        <div class="user" v-if="userInfo">
+          <img class="avatar" :src="userInfo.avatar">
+          <p class="username">{{userInfo.username}}</p>
+        </div>
+        <div v-else class="cover">
+          <img class="avatar blank" src="/static/images/avatar.png">
+        </div>
     </div>
   </div>
 </template>
@@ -11,7 +16,10 @@
 export default {
   data () {
     return {
-      userInfo: ''
+      userInfo: {
+        username: '周冬雨',
+        avatar: 'https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p36798.jpg'
+      }
     }
   }
 }
@@ -22,17 +30,24 @@ export default {
         height: 220px
         background-color: #2ABE7B
         position relative
-        .cover
+        .user, .cover
+            margin: auto
             position: absolute
-            width: 80px
-            height: 80px
-            left: 50%
-            margin-left: -40px
-            top: 40px
-            border-radius: 50%
-            background-color: #ffffff
-            color:#575757
             text-align: center
-            line-height: 80px
-
+            left: 0
+            right: 0
+            top: 30px
+            bottom: 0
+            .avatar
+                background-color: #ffffff
+                width: 80px
+                height: 80px
+                border-radius: 50%
+                &.blank
+                    box-sizing: border-box
+                    border: 20px solid #ffffff
+            .username
+                margin-top: 10px
+                color: #ffffff
+                font-size: 14px
 </style>
